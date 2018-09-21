d7afca9874f0728e26c4e5d2ffcb42a1ea7a1f15
j-shilling <shilling.jake@gmail.com>
2018-09-21 16:48:05 -0400

    Adding solution modules that pass the validation tests

---
+++ app/models/tag.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Tag < ActiveRecord::Base
+  has_many :dish_tags
+  has_many :dishes, through: :dish_tags
+
+  validate :name, :name_validator
+
+  private
+
+  def name_validator
+    if name == nil || name.empty?
+      errors.add(:name, "can't be presence")
+    elsif name.size < 3
+      errors.add(:name, "must be 3 or more characters")
+    elsif name.scan(/\w+/).size > 2
+      errors.add(:name, "can't be more than 2 words")
+    end
+  end
+end
