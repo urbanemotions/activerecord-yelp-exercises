e2d1acada76efa396981e0e9d1dc51d56299bec1
j-shilling <shilling.jake@gmail.com>
2018-09-21 16:48:05 -0400

    Adding solution modules that pass the validation tests

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,26 +1,9 @@
 class Dish < ActiveRecord::Base
   belongs_to :restaurant
   has_many :dish_tags
-  has_many :dish_orders
   has_many :tags, through: :dish_tags
-  has_many :orders, through: :dish_orders
-
-  scope :with_tag, -> (name) { joins(:tags).merge(Tag.with_name(name)) }
-  scope :without_tag, -> (name) { where.not(id: with_tag(name)) }
-  scope :vegetarian, -> { with_tag("vegetarian") }
-  scope :non_veggie, -> { without_tag("vegetarian") }
-  scope :names, -> { pluck(:name) }
-  scope :max_tags, -> { joins(:dish_tags).group(:dish_id).order("COUNT(dish_id) DESC").take }
-  scope :tagged, -> { joins(:tags) }
-  scope :untagged, -> { where.not(id: tagged) }
-  scope :average_tag_count, -> { average(:tag_count) }
 
   validates :name, presence: true
   validates :restaurant, presence: true
-  validates_associated :dish_tags, :dish_orders
-
-  def tag_names
-    tags.pluck(:name)
-  end
-
+  validates_associated :dish_tags
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,46 +1,5 @@
 class Restaurant < ActiveRecord::Base
   has_many :dishes
-  has_many :reviews
-  has_many :orders
-  has_many :customers, through: :orders
 
   validates :name, presence: true
-
-  scope :with_dish_with_tag, -> (name) { joins(:dishes).merge(Dish.with_tag(name)).uniq }
-  scope :with_vegetarian_dish, -> { with_dish_with_tag("vegetarian") }
-  scope :with_dish_without_tag, -> (name) { joins(:dishes).merge(Dish.without_tag(name)).uniq }
-  scope :with_non_vegetarian_dish, -> { with_dish_without_tag("vegetarian") }
-  scope :without_dishes_without_tag, -> (name) { where.not(id: with_dish_without_tag(name)) }
-  scope :vegetarian, -> { without_dishes_without_tag("vegetarian") }
-  scope :name_like, -> (name) { where("name LIKE ?", "%#{name}%") }
-  scope :name_not_like, -> (name) { where.not(id: name_like(name)) }
-
-  def self.mcdonalds
-    Restaurant.find_by(:name => "McDonalds")
-  end
-
-  def self.tenth
-    Restaurant.find(10)
-  end
-
-  def self.with_long_names
-    where('LENGTH(name) > 12')
-  end
-  
-  def self.focused
-    joins(:dishes).having('COUNT(dishes.id) < 5').group(:restaurant_id)
-  end
-
-  def self.large_menu
-    joins(:dishes).having('COUNT(dishes.id) > 20').group(:restaurant_id)
-  end
-
-  def most_popular_tag
-    Tag.joins(:dishes)
-       .where(dishes: {:restaurant_id => self.id})
-       .group(:tag_id)
-       .order('COUNT(restaurant_id) DESC')
-       .take
-  end
-
 end
