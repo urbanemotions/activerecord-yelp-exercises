b581744d2d8f2e532172252897aac8b1c3434e46
j-shilling <shilling.jake@gmail.com>
2018-09-24 13:32:33 -0400

    Implementing and testing Restaurant.mcdonalds and Restaurant.tenth

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -23,5 +23,25 @@ describe 'Resaurant' do
     expect(Restaurant.new(:name => nil).valid?).to be false
     expect(Restaurant.new(:name => "Alice's Restaurant").valid?).to be true
   end
+
+  describe 'Restaurant.mcdonalds' do
+
+    it 'finds the restaurant with the name "McDonalds"' do
+      mcdonalds = Restaurant.create(name: "McDonalds")
+      expect(Restaurant.mcdonalds.id).to eq(mcdonalds.id)
+    end
+
+  end
+
+  describe 'Restaurant.tenth' do
+
+    it 'finds the tenth restaurant' do
+      for i in 1..10
+        Restaurant.create(:name => "restaurant_#{i}")
+      end
+
+      expect(Restaurant.tenth.id).to eq(10)
+    end
+  end
     
 end
