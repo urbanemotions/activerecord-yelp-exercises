37f3715fab3283059657b9a86e38ddb8c967d790
j-shilling <shilling.jake@gmail.com>
2018-09-24 13:32:33 -0400

    Implementing and testing Restaurant.mcdonalds and Restaurant.tenth

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,16 @@ class Restaurant < ActiveRecord::Base
   has_many :dishes
 
   validates :name, presence: true
+
+  def self.mcdonalds
+    Restaurant.find_by(:name => "McDonalds")
+  end
+
+  def self.tenth
+    begin
+      Restaurant.find(10)
+    rescue ActiveRecord::RecordNotFound => e
+      nil
+    end
+  end
 end
