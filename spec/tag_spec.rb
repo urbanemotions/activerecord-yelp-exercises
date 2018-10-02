03d24e30539886d7cccabda8cfe6cb848c0051cb
j-shilling <shilling.jake@gmail.com>
2018-10-02 12:53:09 -0400

    FIXED: dishes created in the Tag.most_common test were not valid

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -2,13 +2,14 @@ require_relative 'spec_helper'
 
 describe 'Tag' do 
 
+  let(:alices) {Restaurant.create(:name => "Alice's Restaurant")}
+
   it "has a name" do 
     tag = Tag.create(:name => "italian")
     expect(tag.name).to eq ("italian")
   end
     
   it "has associated dishes in an array" do 
-    alices  = Restaurant.create(:name => "Alice's Restaurant")
     pizza   = Dish.create(:name => "pizza", :restaurant => alices)
     italian = Tag.create(:name => "italian", :dishes => [pizza])
     
@@ -33,6 +34,27 @@ describe 'Tag' do
     expect(Tag.new(:name => "one two three").valid?).to be false 
     expect(Tag.new(:name => "one two").valid?).to be true 
     expect(Tag.new(:name => "one").valid?).to be true
+  end
+
+  describe "Tag.most_common" do
+
+    it "returns the tag with the most associated dishes" do
+      three = Tag.create(:name => "three")
+      two   = Tag.create(:name => "two")
+      one   = Tag.create(:name => "one")
+
+      for i in 1..3 do
+        Dish.create(:name => "dish#{i}", :tags => [three], :restaurant => alices)
+      end
+
+      for i in 1..2 do
+        Dish.create(:name => "dish#{i+3}", :tags => [two], :restaurant => alices)
+      end
+
+      Dish.create(:name => "dish6", :tags => [one], :restaurant => alices)
+      expect(Tag.most_common).to eq(three)
+    end
+
   end
   
 end
