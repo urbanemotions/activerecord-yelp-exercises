29716d250a5f59f70356a749cc820cabe616a08f
j-shilling <shilling.jake@gmail.com>
2018-10-09 13:20:21 -0400

    Cleaning up dish_spec

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -20,80 +20,94 @@ describe 'Dish' do
     end
   end
 
-  let(:alices_restaurant) {Restaurant.create(:name => "Alice's Restaurant")}
+  describe 'validations' do
+    let(:alices) { Restaurant.find_or_create_by(name: "Alice's Restaurant") }
 
-  let(:italian) {Tag.create(:name => "italian")}
-  let(:vegetarian) {Tag.create(:name => "vegetarian")}
-
-  let!(:pizza) {Dish.create(:name => "pizza", :tags => [italian], :restaurant => alices_restaurant)}
-  let!(:pasta) {Dish.create(:name => "pasta", :tags => [italian, vegetarian], :restaurant => alices_restaurant)}
-  let!(:soda)  {Dish.create(:name => "soda", :restaurant => alices_restaurant)}
-  let!(:water) {Dish.create(:name => "water", :restaurant => alices_restaurant)}
-
-  it "validates that name is present" do 
-    expect(Dish.new(:name => nil, :restaurant => alices_restaurant).valid?).to be false
-    expect(Dish.new(:name => "Pizza", :restaurant => alices_restaurant).valid?).to be true
-  end
-  
-  it "validates that restaurant is present" do 
-    expect(Dish.new(:name => "Pizza", :restaurant => nil).valid?).to be false
-    expect(Dish.new(:name => "Pizza", :restaurant => alices_restaurant).valid?).to be true
-  end
-  
-  it "invokes the validations associated with DishTag" do 
-    # Somehow we need to verify here that the valitations on DishTag are actually
-    # checked whenever we add a tag to a dish.
-  end
-
-  describe 'Dish.names' do
-
-    it 'returns an array of all dish names' do
-      expect(Dish.names).to contain_exactly("pizza", "pasta", "soda", "water")
+    it "must have a name" do
+      expect(Dish.new(name: nil, restaurant: alices).valid?).to be false
+      expect(Dish.new(name: "name", restaurant: alices).valid?).to be true
     end
 
+    it 'must have a restaurant' do
+      expect(Dish.new(:name => "Pizza", :restaurant => nil).valid?).to be false
+      expect(Dish.new(:name => "Pizza", :restaurant => alices).valid?).to be true
+    end
+
+    it 'invokes validations for associated DishTags' do
+      # Somehow we need to verify here that the valitations on DishTag are actually
+      # checked whenever we add a tag to a dish.
+    end
   end
 
-  describe 'Dish.max_tags' do
+  describe 'queries' do
+    before(:all) do
+      @alices = Restaurant.find_or_create_by(name: "Alice's Restaurant")
+      @dish_hash = {
+        burger: ["meat", "fat", "meal"],
+        fry: ["vegetarian", "side", "potato", "salty"],
+        soda: ["sugar", "vegetarian"],
+        taco: ["vegetarian"],
+        water: [],
+        ice: []
+      }
 
-    it 'returns a signle dish with the most tags' do
-      expect(Dish.max_tags).to eq(pasta)
+      @dish_hash.each do |dish, tags|
+        d = Dish.create(name: dish, restaurant: @alices)
+        tags.each do |tag|
+          d.tags << Tag.find_or_create_by(name: tag)
+        end
+      end
     end
 
-  end
-
-  describe 'Dish.untagged' do
-
-    it "returns an array of dishes with no tags" do
-      expect(Dish.untagged).to contain_exactly(soda, water)
+    describe 'Dish.names' do
+      it 'returns an array of all dish names' do
+        expect(Dish.names).to match_array @dish_hash.keys.map(&:to_s)
+      end
     end
 
-  end 
-
-  describe 'Dish.average_tag_count' do
-
-    it "returns the average tag count for all dishes" do
-      expect(Dish.average_tag_count).to eq(0.75)
+    describe 'Dish.max_tags' do
+      it 'returns a signle dish with the most tags' do
+        max_tags = @dish_hash.max_by { |entry| entry[1].length }
+        expected = Dish.find_by(name: max_tags[0].to_s)
+        expect(Dish.max_tags).to eq(expected)
+      end
     end
 
-  end
-
-  describe 'Dish#tag_count' do
-
-    it "returns the number of tags a dish has" do
-      expect(pizza.tag_count).to eq(1)
-      expect(pasta.tag_count).to eq(2)
-      expect(soda.tag_count).to  eq(0)
+    describe 'Dish.untagged' do
+      it 'returns an array of dishes with no tags' do
+        expected = @dish_hash.select { |key, value| value.length == 0 }
+                             .map { |key, value| Dish.find_by(name: key.to_s) }
+        expect(Dish.untagged).to match_array(expected)
+      end
     end
 
-  end
-
-  describe 'Dish#tag_names' do
-
-    it "returns an array of the names of each tag associated with this dish" do
-      expect(pasta.tag_names).to contain_exactly("vegetarian", "italian")
-      expect(pizza.tag_names).to contain_exactly("italian")
+    describe 'Dish.average_tag_count' do
+      it 'returns the average tag count for all dishes' do
+        total_tags = @dish_hash.reduce(0) do |sum, (dish, tags)|
+          sum += tags.length
+        end
+        expected = total_tags.to_f / @dish_hash.keys.count.to_f
+        expect(Dish.average_tag_count).to be_within(1e-12).of(expected)
+      end
     end
 
+    describe 'Dish#tag_count' do
+      it 'returns the number of tags associated with this dish' do
+        @dish_hash.each do |dish, tags|
+          d = Dish.find_by(name: dish)
+          expect(d.tag_count).to eq(tags.length)
+        end
+      end
+    end
+ 
+    describe 'Dish#tag_names' do
+      it 'returns an array of the names of each tag associated with this dish' do
+        @dish_hash.each do |dish, tags|
+          d = Dish.find_by(name: dish)
+          expect(d.tag_names).to match_array(tags)
+        end
+      end
+    end
   end
 
 end
