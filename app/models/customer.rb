5a54edb224136ced8fab5e10ca78858f3d781f75
j-shilling <shilling.jake@gmail.com>
2018-10-15 09:46:20 -0400

    creating customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,2 @@
 class Customer < ActiveRecord::Base
-  has_many :orders
-  has_many :restaurants, through: :orders
-  has_many :reviews
-
-  validates :name, :lat, :lon, presence: true
-  validates :lat, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
-  validates :lon, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 end
