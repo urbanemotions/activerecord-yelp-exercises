8203246afa970135483daef54c63a552ff33fcfc
j-shilling <shilling.jake@gmail.com>
2018-10-15 10:03:33 -0400

    Adding Customer's associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,2 @@
 class Order < ActiveRecord::Base
-  belongs_to :customer
-  belongs_to :restaurant
-  has_many :dish_orders
-  has_many :dishes, through: :dish_orders
-
-  validates :customer, :restaurant, presence: true
-  validates :dishes, length: {minimum: 1}
-  validates_associated :dish_orders
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,20 +1,2 @@
 class Review < ActiveRecord::Base
-  belongs_to :customer
-  belongs_to :restaurant
-
-  validates :restaurant, presence: true
-  validates :content, presence: true, length: {minimum: 10}
-  validates :rating, presence: true, inclusion: {in: 1..5}
-
-  validate  :customer, :customer_validator
-
-  private
-
-  def customer_validator
-    if customer == nil
-      errors.add(:customer, "must be present")
-    elsif !customer.restaurants.include?(restaurant)
-      errors.add(:customer, "must have made an order at this restaurant")
-    end
-  end
 end
