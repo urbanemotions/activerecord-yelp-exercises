cdc7ff7f4ec62b5f3742adf2a02b3c1e70420d1b
j-shilling <shilling.jake@gmail.com>
2018-10-15 10:13:39 -0400

    Adding Order relationships

--- app/models/dish_order.rb
+++ app/models/dish_order.rb
@@ -1,14 +1,4 @@
 class DishOrder < ActiveRecord::Base
-  validate :restaurants_match
-
   belongs_to :dish
   belongs_to :order
-
-  private
-
-  def restaurants_match
-    if dish.restaurant != order.restaurant
-      errors.add(:dish, "dish and order must be from the same restaurant")
-    end
-  end
 end
