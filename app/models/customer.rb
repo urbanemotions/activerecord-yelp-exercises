c179df45ec4fd8f3238eda6fb6c81adcb52a68ab
j-shilling <shilling.jake@gmail.com>
2018-10-15 14:17:18 -0400

    Validating Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,7 @@ class Customer < ActiveRecord::Base
   has_many :orders
   has_many :reviews
 
-  validates :name, presence: true
+  validates :name, :lat, :lon, presence: true
+  validates :lat, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :lon, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 end
