46fedfa70cb7ef4ada6bfd56056ca0063c0bad08
j-shilling <shilling.jake@gmail.com>
2018-10-15 14:33:35 -0400

    Validating Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ActiveRecord::Base
   belongs_to :restaurant
   has_many :dish_orders
   has_many :dishes, through: :dish_orders
+
+  validates :customer, :restaurant, presence: true
+  validates :dishes, length: {minimum: 1}
 end
